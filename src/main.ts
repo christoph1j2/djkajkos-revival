5014d84b47d519883513c35ba8e1eb67cfa1b30a
Ernst Christoph Leschka <ernstleska@gmail.com>
2024-11-21 19:59:08 +0100

    added music module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CommandsService } from 'necord';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const commandService = app.get(CommandsService);
+
+  try {
+    await commandService.registerAllCommands();
+    Logger.log('Global commands registered successfully');
+
+    const GUILD_ID = '674273672524988440'; // Replace with your server ID
+    if (GUILD_ID) {
+      await commandService.registerInGuild(GUILD_ID);
+      Logger.log(`Guild commands registered in guild ID: ${GUILD_ID}`);
+    }
+  } catch (error) {
+    Logger.error('Error registering commands', error);
+  }
+
+  await app.listen(3000);
 }
 bootstrap();
